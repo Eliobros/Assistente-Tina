e7cb509d135fb63637988a9aaae3485d7b417df4
Luisa Dev <eliobrostech3@gmail.com>
2024-11-11 17:40:58 +0200

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // Aqui você pode adicionar validação ou envio dos dados ao servidor, se necessário
 
     // Após o login, redireciona para a página "index.html" ou outra página que você desejar
-    window.location.href = "index.html"; // Aqui você coloca o caminho para o arquivo desejado
+    window.location.href = "chat.html"; // Aqui você coloca o caminho para o arquivo desejado
 
     // Caso queira esconder o formulário e mostrar o chat, ainda é possível usar:
     // document.getElementById('form-container').style.display = 'none';
